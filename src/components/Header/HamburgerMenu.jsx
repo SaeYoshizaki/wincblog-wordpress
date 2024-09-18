01240b945265fa6bce0ec72c6516c79a4342b8f1
SaeYoshizaki <yoshizakisae@gmail.com>
2024-09-18 18:28:39 +0900

    initial commit

--- src/components/Header/HamburgerMenu.jsx
+++ src/components/Header/HamburgerMenu.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './HamburgerMenu.css'; 
+import { useState } from "react";
+import "./HamburgerMenu.css";
 
 export default function HamburgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,14 +15,26 @@ export default function HamburgerMenu() {
         <div className="line"></div>
         <div className="line"></div>
       </div>
-      <nav className={`menu ${isOpen ? 'open' : ''}`}>
+      <nav className={`menu ${isOpen ? "open" : ""}`} style={{ zIndex: 999 }}>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Web</a></li>
-          <li><a href="/">App</a></li>
-          <li><a href="/">Security</a></li>
-          <li><a href='/articles/article'>Tech Blogs</a></li>
-          <li><a href='/articles/users'>Members</a></li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/web">Web</a>
+          </li>
+          <li>
+            <a href="/app">App</a>
+          </li>
+          <li>
+            <a href="/competitive_programming">Competitive Programing</a>
+          </li>
+          <li>
+            <a href="/articles/article">Tech Blogs</a>
+          </li>
+          <li>
+            <a href="/articles/users">Members</a>
+          </li>
         </ul>
       </nav>
     </div>
