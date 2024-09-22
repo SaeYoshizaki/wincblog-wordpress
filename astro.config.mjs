88488257e9e7ccbefb8db3f065f23d015daac185
SaeYoshizaki <yoshizakisae@gmail.com>
2024-09-22 11:54:47 +0900

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [react()],
+
   vite: {
     ssr: {
       noExternal: ['microcms-js-sdk'],  // microcms-js-sdkを外部依存として扱わない
     },
   },
+
+  output: 'server',
+  adapter: vercel(),
 });
