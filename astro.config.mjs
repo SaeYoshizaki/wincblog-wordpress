1aa5ad2773d56cb1bb5dd4b89ca2b0ba82a632ba
SaeYoshizaki <yoshizakisae@gmail.com>
2024-09-22 12:13:59 +0900

    fix errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-import vercel from '@astrojs/vercel';
-
+import vercel from '@astrojs/vercel/serverless';
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    ssr: {
-      noExternal: ['microcms-js-sdk'],  // microcms-js-sdkを外部依存として扱わない
-    },
-  },
-
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
