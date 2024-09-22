fa4b9f77a54cab0e487e56f730dd0ce6ec2f601e
SaeYoshizaki <yoshizakisae@gmail.com>
2024-09-22 10:49:11 +0900

    fixing errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [react()],
-  site: 'https://SaeYoshizaki.github.io',
   vite: {
     ssr: {
       noExternal: ['microcms-js-sdk'],  // microcms-js-sdkを外部依存として扱わない
