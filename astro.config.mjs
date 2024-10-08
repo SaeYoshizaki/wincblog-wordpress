bc983ef0eaede41f64569cef05c80048a3b70047
SaeYoshizaki <yoshizakisae@gmail.com>
2024-10-08 10:11:45 +0900

    fixing errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    ssr: {
+      noExternal: ['microcms-js-sdk'],  // microcms-js-sdkを外部依存として扱わない
+    },
+  },
+  site: 'https://saeyoshizaki.github.io/',
+  outDir: "./docs",
+  base: '/wincblog-wordpress'
 });
