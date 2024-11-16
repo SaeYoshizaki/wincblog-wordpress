e1272e2f467af313b143700f54371024290aa35f
SaeYoshizaki <yoshizakisae@gmail.com>
2024-11-16 17:29:26 +0900

    fixing some errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,4 @@ export default defineConfig({
       noExternal: ['microcms-js-sdk'],  // microcms-js-sdkを外部依存として扱わない
     },
   },
-  site: 'https://saeyoshizaki.github.io/',
-  outDir: "./docs",
-  base: '/wincblog-wordpress'
 });
